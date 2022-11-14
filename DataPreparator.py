eb6f219e7590f638d4b6bd752d9d7b5a541b8e13
EskildAndersen <202007483@post.au.dk>
2022-11-14 23:24:21 +0100

    lidt ændringer til pp og lidt til dataclassen

--- DataPreparator.py
+++ DataPreparator.py
@@ -2,6 +2,7 @@
 
 Script to generate data class
 
+This script assusmes that path is structured as mentioend in readme.
 
 '''
 
@@ -24,12 +25,11 @@ import matplotlib.pyplot as plt
 
 class ImageDataset(torch.utils.data.Dataset):
     def __init__(self, annotations_file, img_dir, transform=None, target_transform=None):
-        self.img_labels = pd.read_csv(os.getcwd(
-        ) + annotations_file, sep='#\d+\t', header=None, names=['path', 'text']).reset_index(drop=True)
+        self.img_labels = pd.read_csv(annotations_file, sep='#\d+\t', header=None, names=['path', 'text']).reset_index(drop=True)
         self.img_dir = img_dir
 
-        # Resize all pictures into the same dimensions
-        self.transform = torchvision.transforms.Resize([300, 300])
+        # Resize all pictures into the same dimensions. (probably a bit sus)
+        self.transform = torchvision.transforms.Resize([500, 500])
         self.target_transform = target_transform
 
     def __len__(self):
@@ -39,7 +39,7 @@ class ImageDataset(torch.utils.data.Dataset):
 
         # We define the image path and the corresponding pandas column with the filename:
         img_path = os.path.join(self.img_dir, self.img_labels.iloc[idx, 0])
-        image = read_image(os.getcwd() + img_path)
+        image = read_image(img_path)
 
         # The pandas column containing the labels
         label = self.img_labels.iloc[idx, 1]
@@ -49,13 +49,13 @@ class ImageDataset(torch.utils.data.Dataset):
             label = self.target_transform(label)
         return image, label
 
+# Define the paths for labels and imagss
+txtFilePath = 'data/texts/Flickr8k.token.txt'
+imgPath = 'data/images/'
 
-txtFilePath = '/data/texts/Flickr8k.token.txt'
-imgPath = '/data/images/'
-
+# Initialzie dataset class and into the loader. 
 Dataset = ImageDataset(txtFilePath, imgPath)
 data_loader = DataLoader(Dataset, batch_size=1, shuffle=True)
-
 
 # get some images
 dataiter = iter(data_loader)
@@ -66,6 +66,7 @@ for image, label in zip(images, labels):  # Run through all samples in a batch
     plt.figure()
     plt.imshow(np.transpose(image.numpy(), (1, 2, 0)))
     plt.title(label)
+    plt.axis('off')
     plt.show()
 
-#texts = pd.read_csv(os.getcwd() + txtFilePath, sep= '\t', header =None, names = ['path', 'text']).reset_index(drop = True)
+#
