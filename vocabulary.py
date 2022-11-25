af5842b267bde3cf2f7074976530ebabd35d62ad
Simon Rydder <simonrydder@gmail.com>
2022-11-25 08:54:39 +0100

    updated vocab with EOS, SOS and PAD

--- vocabulary.py
+++ vocabulary.py
@@ -10,15 +10,20 @@ with open(_file) as f:
 
 _sentences = [line.split('\t')[1].strip().lower() for line in _lines]
 
+max_len = max([len(s.split(' ')) for s in _sentences]) + 2
+example = _sentences[0]
+print(max_len)
+
 _words = {w for s in _sentences for w in s.split(' ')}
-print(len(_words))
+_words |= {'<EOS>', '<SOS>', '<PAS>'}
 
 vocab = {w : i for i, w in enumerate(_words)}
-
 inv_vocab = {i : w for w, i in vocab.items()}
 
 _s1 = time.time()
 
-print(_s1-_s0)
+if __name__ == '__main__':
+    print(len(_words))
+    print(_s1-_s0)
 
 
